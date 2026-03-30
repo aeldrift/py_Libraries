ab1cda631d4719e1e481b6c06ab01836cf04c6b1
Teesha <jindalteesha22@gmail.com>
2026-03-30 19:30:35 +0530

    added

--- Numpy/practice.py
+++ Numpy/practice.py
@@ -36,11 +36,40 @@ print(three_D_array[1][1][0]) # F
 print("using Multidimensional Idexing ",three_D_array[0,0,0]) # A
 
 # Multidimensional Indexing is faster than Chain Indexing 
-
-word = three_D_array[0,0,0] + three_D_array[1,2,1] + three_D_array[2,2,2]
+ 
+word = three_D_array[0,0,0] + three_D_array[1,2,1] + three_D_array[2,2,2] # The words are concatinated using indexing
 print(word)
 
+# Slicing
 
+array = np.array([[1,2,3,4], # idx: 0
+                  [5,6,7,8], # idx: 1
+                  [9,10,11,12], # idx: 2
+                  [13,14,15,16]]) # idx: 3
+# array(Start:End:Step)
+print(array[0]) # 1,2,3,4
+print(array[1]) # 5,6,7,8
+print(array[2]) # 9,10,11,12
+
+# Or can use the negative indexing as:
+print("-ve indexed slicing:", array[-1]) # 13,14,15,16
+
+print("-ve indexed slicing:", array[-1]) # 13,14,15,16
+print("-ve indexed slicing:", array[-2]) # 9,10,11,12
+
+# End
+
+print(array[0:4]) # last index is EXCLUDED
+print(array[1:3]) # last index is EXCLUDED
+print(array[0:]) # will select everything to the end
+
+# Step
+
+print("start:end:step\n", array[0:4:2])
+print("start:end:step\n", array[::2]) # To acess all the indices
+
+print("start:end:-ve step\n", array[::-1]) # To print all the rows reversed
+print("start:end:-ve step\n", array[::-2]) # To print all the rows reversed and step every 2nd row
 
 
  
