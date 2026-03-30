a7fdd7f90f1cb5141bce02bbf649695f64f0029c
Teesha <jindalteesha22@gmail.com>
2026-03-30 18:31:40 +0530

    added

---
+++ Numpy/practice.py
@@ -0,0 +1,8 @@
+# to print the version of Numpy
+# Numpy: Fast numerical computation using arrays instead of lists   
+import numpy as np
+print(np.__version__)  #2.4.1
+
+my_list = [1,2,3,4,5]
+my_list = my_list * 2
+print(my_list) # Instead of multiplying no's with 2, will multiply items 2 times
