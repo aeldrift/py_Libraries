52c497a5de408ba85b590d1fc3cf4a58dd38d3a8
Teesha <jindalteesha22@gmail.com>
2026-04-12 15:01:49 +0530

    added

--- pandas/practice.py
+++ pandas/practice.py
@@ -1,2 +1,9 @@
 import pandas as pd
-print(pd.__version__)
+print(pd.__version__) # To check the version of the pandas 
+
+# Series: A pandas 1-D Labeled array that can hold any data type
+        # Think of it like a single column in a spreadsheet (1-Dimensional)
+
+data = [100,200,300]
+series = pd.Series(data)
+print(series)
