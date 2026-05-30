378f032304689322fa0b82282fd387f40b8acf1e
Teesha <jindalteesha22@gmail.com>
2026-05-30 16:54:24 +0530

    added

--- pandas/data_frames.py
+++ pandas/data_frames.py
@@ -2,15 +2,25 @@ import pandas as pd
 # Dataframe:  A tabular data structure with rows and columns. (2 Dimensional)
 # similar to excel spreadsheet 
 
-data = { "Name": ["Aman", "Anju", "Patrick", "Meenakshi"], "Age": [30, 35, 50, 20] }
+data = { "Name": ["Aman", "Anju", "Patrick"], "Age": [30, 35, 50] }
 
 df = pd.DataFrame(data)
 print(df)
 
-df = pd.DataFrame(data, index= ["Employee 1", "Employee 2", "Employee 3", "Employee 4"])
+df = pd.DataFrame(data, index= ["Employee 1", "Employee 2", "Employee 3"])
 print(df)
 
 print(df.loc["Employee 1"])
 print(df.loc["Employee 2"])
 
 print(df.iloc[2])
+
+# To add a new column 
+df["Job"] = ["Cook", "Cashier", "Driver"]
+print(df)
+
+# To add a new row
+print("Adding a new row")
+new_row = pd.DataFrame([{"Name": "Sandy",  "Age": 28, "Job": "Engineer" }, {"Name": "managaer",  "Age": 28, "Job": "Peon" }], index = ["Employee 4", "Employee 5"])
+df = pd.concat([df, new_row])
+print(df)
