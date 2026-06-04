791699e11be8ce8d3e956288f000aec27e194e79
Teesha <jindalteesha22@gmail.com>
2026-06-04 22:43:58 +0530

    Update practice.py

--- pandas/practice.py
+++ pandas/practice.py
@@ -1,6 +1,6 @@
 import pandas as pd # Import pandas to use it and using pd as an alias
 print(pd.__version__) # To check the version of the pandas 
- 
+   
 # Series: A pandas 1-D Labeled array that can hold any data type
         # Think of it like a single column in a spreadsheet (1-Dimensional)
 
